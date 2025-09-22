5d1fc3b707a0ff8fcc76c6bf1308ec29f3de90bc
hana67277 <hfabdulhameed@gmail.com>
2025-09-22 22:42:03 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { NotFound } from "./pages/NotFound";
+import {Toaster} from "@/components/ui/toaster";
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+     <Toaster/>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
+
+
+
